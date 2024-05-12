2fc1ee70d76bfdfa2b3f98dd133eb72b6a912cbf
Your Name <you@example.com>
2024-05-12 23:14:09 +0700

    first commit

---
+++ screens/Profile.js
@@ -0,0 +1,13 @@
+
+import {View, Text} from 'react-native'
+const Profile = () =>{
+    return(
+        <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+            <Text>
+            Profile screen
+            </Text>
+        </View>
+    )
+}
+
+export default Profile;
